f1df642187c191ddd96c240244614f29db444495
Olim Yuldoshev <oyuldoshev39@gmail.com>
2024-10-17 11:53:16 +0500

    Added the functionality of the link

--- src/components/linksMobileSize/LinksMobileSize.tsx
+++ src/components/linksMobileSize/LinksMobileSize.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 type ILinksMobileSize = {
   pageLink: string;
@@ -11,9 +11,34 @@ const LinksMobileSize: React.FC<ILinksMobileSize> = ({
   page,
   closeMenuMobileSize,
 }) => {
+  const [currentHash, setCurrentHash] = useState(window.location.hash);
+
+  // const currentHash = window.location.hash;
+
+  useEffect(() => {
+    // Event listener for hash changes
+    const onHashChange = () => {
+      setCurrentHash(window.location.hash);
+    };
+
+    window.addEventListener("hashchange", onHashChange);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("hashchange", onHashChange);
+    };
+  }, []);
+
   return (
     <a className="" href={pageLink} onClick={closeMenuMobileSize}>
-      <li className="text-center hover:bg-sky-500 py-1 text-[#989898] hover:text-[#fff] dark:text-[gray] dark:hover:text-[#fff] active:text-[#fff] active:dark:text-[#fff] duration-[0.5s] cursor-pointer hover:underline rounded-[5px]">
+      <li
+        className={`text-center py-1 text-[#989898] dark:text-[gray] dark:hover:text-[#fff] active:text-[#fff] active:dark:text-[#fff] duration-[0.5s] cursor-pointer hover:underline rounded-[5px]
+          ${
+            pageLink === currentHash
+              ? `bg-sky-500 text-[#fff] underline`
+              : `hover:bg-sky-500 hover:text-[#fff]`
+          }`}
+      >
         <a
           className="text-[20px] font-[400]"
           href={pageLink}
