a29185408f01aa6b24a40394a67ea64a3ade5b0a
Olim Yuldoshev <oyuldoshev39@gmail.com>
2024-10-17 11:09:14 +0500

    Fixed some problems

--- src/components/linksMobileSize/LinksMobileSize.tsx
+++ src/components/linksMobileSize/LinksMobileSize.tsx
@@ -3,16 +3,16 @@ import React from "react";
 type ILinksMobileSize = {
   pageLink: string;
   page: string;
-  closeMenuMobileSize: () => void
+  closeMenuMobileSize: () => void;
 };
 
-const LinksMobileSize:React.FC<ILinksMobileSize> = ({ pageLink, page, closeMenuMobileSize }) => {
+const LinksMobileSize: React.FC<ILinksMobileSize> = ({
+  pageLink,
+  page,
+  closeMenuMobileSize,
+}) => {
   return (
-    <a 
-      className=""
-      href={pageLink}
-      onClick={closeMenuMobileSize}
-    >
+    <a className="" href={pageLink} onClick={closeMenuMobileSize}>
       <li className="text-center hover:bg-sky-500 py-1 text-[#989898] hover:text-[#fff] dark:text-[gray] dark:hover:text-[#fff] active:text-[#fff] active:dark:text-[#fff] duration-[0.5s] cursor-pointer hover:underline rounded-[5px]">
         <a
           className="text-[20px] font-[400]"
